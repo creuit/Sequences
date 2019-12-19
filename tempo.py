672e5acacf02015a08559a81a677de6cce992523
creuit <ikilisavgroeb@tutanota.com>
2019-12-19 20:13:32 +0200

    series of all the music tempos

---
+++ tempo.py
@@ -0,0 +1,22 @@
+tempo = 40
+print(tempo)
+
+while tempo < 60:
+    tempo += 2
+    print(tempo)
+
+while tempo < 72:
+    tempo += 3
+    print(tempo)
+
+while tempo < 120:
+    tempo += 4
+    print(tempo)
+
+while tempo < 144:
+    tempo += 6
+    print(tempo)
+
+while tempo < 208:
+    tempo += 8
+    print(tempo)
