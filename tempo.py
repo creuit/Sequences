7db4eec6f8ba430cb45cc5f3c42705bfe49d463f
creuit <32823576+creuit@users.noreply.github.com>
2019-12-19 21:03:29 +0200

    define function
    function

--- tempo.py
+++ tempo.py
@@ -1,22 +1,15 @@
+def tmp(step, stop):
+    while tempo < stop:
+        tempo += step
+        print(tempo)
+
 tempo = 40
 print(tempo)
 
-while tempo < 60:
-    tempo += 2
-    print(tempo)
+tmp(2, 60)
+tmp(3, 72)
+tmp(4, 120)
+tmp(6, 144)
+tmp(8, 208)
 
-while tempo < 72:
-    tempo += 3
-    print(tempo)
 
-while tempo < 120:
-    tempo += 4
-    print(tempo)
-
-while tempo < 144:
-    tempo += 6
-    print(tempo)
-
-while tempo < 208:
-    tempo += 8
-    print(tempo)
